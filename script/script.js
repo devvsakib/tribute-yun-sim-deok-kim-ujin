ea59a059841b9b6b951418b76bd8934cf909f196
DevvSakib <devvsakib@gmail.com>
2023-01-31 21:25:35 +0600

    lyrics while audio play added

--- script/script.js
+++ script/script.js
@@ -26,3 +26,8 @@ navItems.forEach(item => {
         }
     })  
 });
+
+new RabbitLyrics({
+    element: document.findElementById('lyrics-1'),
+    mediaElement: document.findElementById('audio-1')
+})
